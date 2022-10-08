be0ef8544edfaa702cc23dcb91d600787ed88443
Kiran-raj <santoshrajpurohit89@gmail.com>
2022-10-08 23:21:08 +0530

    make it a function

--- fillomino_general.py
+++ fillomino_general.py
@@ -1,12 +1,23 @@
+import sys
 from tqdm import tqdm
 from itertools import product
-from utils import is_valid_fillomino_game
-from utils import graph3x3
 
-if __name__ == "__main__":
+from utils import gen_graph, print_grid
+from utils import is_valid_fillomino_game
+
+
+def fillomino_finder_naive(side_length, max_size):
+    all_games = []
     count = 0
-    for game in tqdm(product(range(1,10), repeat=9), total=387420489):
-        if is_valid_fillomino_game(game, graph3x3):
+    graphnxn = gen_graph(side_length)
+    for game in tqdm(product(range(1, max_size+1), repeat=side_length*side_length), total=max_size**(side_length*side_length)):
+        if is_valid_fillomino_game(game, graphnxn ,side_length):
+            all_games.append(game)
+            print_grid(game, side_length)
             count += 1
 
     print(f"Total Count: {count}")
+    return all_games
+
+if __name__=="__main__":
+    fillomino_finder_naive(*map(int, sys.argv[1:]) if len(sys.argv) == 3 else (3, 9))

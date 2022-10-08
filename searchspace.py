42b047a846f76c321e4e728ea06c3f3c3cd5edd9
Kiran-raj <santoshrajpurohit89@gmail.com>
2022-10-08 23:33:58 +0530

    Update searchspace code

--- searchspace.py
+++ searchspace.py
@@ -1,38 +1,28 @@
-import sys
 from math import factorial
-from collections import Counter
-from itertools import product
+
+from tqdm import tqdm
+
 from utils import num_partitions_of
 from utils import partitions
-from tqdm import tqdm
 
 if __name__ == "__main__":
     count = 0
     n = 3  # side length of the grid
     m = 9  # the maximum sized polyomino allowed by the grid
-    naive_space = factorial(n * n)
+    permutations_of_grid = factorial(n * n)
 
-    q = n * n // m
-    r = n * n - m * q
-
-    partition_iterators = [partitions(m) for _ in range(q)]
-    if r:
-        partition_iterators += [partitions(r)]
-
-    for parts in tqdm(
-        product(*partition_iterators),
-        total=(num_partitions_of(m) ** q) * num_partitions_of(r),
-    ):
+    for part in tqdm(partitions(n * n), total=(num_partitions_of(n * n))):
+        if max(part) > m:
+            continue
         grid = []
-        for part in parts:
-            for num in part:
-                grid += [num for _ in range(num)]
+        for num in part:
+            grid += [num for _ in range(num)]
 
-        temp = naive_space
-        for duplicate in Counter(grid).keys():
+        temp = permutations_of_grid
+        for duplicate in part:
             temp /= factorial(duplicate)
-            print(f"Had to search through {temp} permutations of the grid")
 
         count += temp
 
-    print(f"Total search space: {count}")
+    print("Total search space using partition idea:", count)
+    print("Total search space using naive method:", m**n**2)

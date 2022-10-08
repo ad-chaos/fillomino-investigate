8e8d434f747133ecf9ea187c352615f0de936054
Kiran-raj <santoshrajpurohit89@gmail.com>
2022-10-08 23:19:34 +0530

    Add Grid Graph generator tester

--- test.py
+++ test.py
@@ -1,6 +1,77 @@
-from utils import bfs, graph3x3, graph4x4
+from pprint import pprint
+from utils import bfs, gen_graph
 
 outcomes = {True: "Passed ✅", False: "Failed ❌"}
+
+graph0x0 = []
+
+graph1x1 = [()]
+
+graph2x2 = [
+    (1, 2),
+    (0, 3),
+    (0, 3),
+    (1, 2),
+]
+
+graph3x3 = [
+    (1, 3),
+    (0, 2, 4),
+    (1, 5),
+    (0, 6, 4),
+    (1, 7, 3, 5),
+    (2, 8, 4),
+    (3, 7),
+    (6, 8, 4),
+    (7, 5),
+]
+
+graph4x4 = [
+    (1, 4),
+    (0, 5, 2),
+    (1, 6, 3),
+    (2, 7),
+    (0, 5, 8),
+    (1, 4, 6, 9),
+    (5, 2, 10, 7),
+    (3, 6, 11),
+    (4, 9, 12),
+    (8, 5, 13, 10),
+    (9, 6, 14, 11),
+    (15, 10, 7),
+    (8, 13),
+    (12, 9, 14),
+    (13, 10, 15),
+    (11, 14),
+]
+
+graph5x5 = [
+    (1, 5),
+    (0, 6, 2),
+    (1, 7, 3),
+    (2, 8, 4),
+    (3, 9),
+    (0, 6, 10),
+    (1, 5, 11, 7),
+    (6, 2, 12, 8),
+    (7, 3, 13, 9),
+    (4, 8, 14),
+    (5, 11, 15),
+    (10, 6, 16, 12),
+    (11, 7, 17, 13),
+    (12, 8, 18, 14),
+    (9, 13, 19),
+    (10, 16, 20),
+    (15, 11, 21, 17),
+    (16, 12, 22, 18),
+    (17, 13, 23, 19),
+    (14, 18, 24),
+    (15, 21),
+    (20, 16, 22),
+    (21, 17, 23),
+    (22, 18, 24),
+    (19, 23),
+]
 
 
 def test_dfs3x3(grid, cases, truth):
@@ -31,7 +102,26 @@ def test_dfs4x4(grid, cases, truth):
             grid_state[node] = result
 
 
+def test_grid_equality(
+    generated: list[tuple[int, ...]], expected: list[tuple[int, ...]]
+):
+    final_result = True
+    for i, j in zip(generated, expected):
+        final_result &= sorted(i) == sorted(j)
+
+    return final_result
+
+
 # tests
+
+print("Testing Grid Graph Generator: ")
+print(outcomes[test_grid_equality(gen_graph(0), graph0x0)])
+print(outcomes[test_grid_equality(gen_graph(1), graph1x1)])
+print(outcomes[test_grid_equality(gen_graph(3), graph3x3)])
+print(outcomes[test_grid_equality(gen_graph(4), graph4x4)])
+print(outcomes[test_grid_equality(gen_graph(5), graph5x5)])
+print()
+
 print("Testing Grid 1: ")
 grid = [2, 5, 5, 2, 5, 2, 5, 5, 2]
 test_dfs3x3(grid, [0, 1, 8], [True, True, True])
@@ -80,6 +170,13 @@ print()
 
 print("Testing Grid 10: ")
 grid = [4, 4, 3, 3, 4, 4, 3, 1, 2, 2, 6, 6, 6, 6, 6, 6]
+"""
+4 4 3 3
+4 4 3 1
+2 2 6 6
+6 6 6 6
+"""
+# breakpoint()
 test_dfs4x4(grid, [0, 3, 6, 1], [True, True, True, True])
 print()
 
@@ -87,3 +184,8 @@ print("Testing Grid 11: ")
 grid = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 test_dfs4x4(grid, [1, 5, 9], [False, False, False])
 print()
+
+# print("Testing Grid 12: ")
+# grid = [6, 6, 6, 6, 5, 6, 5, 6, 5, 5, 5, 2, 2, 1, 1, 1]
+# test_dfs4x4(grid, [11, 15, 0, 5, 12, 13], [False, False, True, True, False, False])
+# print()
